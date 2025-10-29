7827366eb784c717ac8fae796307694e361b055b
ideaqlabs <ideaq.labs@gmail.com>
2025-10-29 23:04:36 +0530

    Update Profile.jsx

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Profile = () => {
   return (
     <section className="flex flex-col items-center justify-center min-h-screen text-center px-6">
-      <h1 className="text-5xl font-bold mb-6 text-yellow-400">START LEARNING</h1>
+      <h1 className="text-5xl font-bold mb-6 text-yellow-400">PROFILE SETTINGS</h1>
       <p className="text-lg text-gray-300 max-w-2xl mb-10">
         Welcome to the Profile section of IdeaQ. Soon you'll be able to access Profle Settings & FAQ features here.
       </p>
